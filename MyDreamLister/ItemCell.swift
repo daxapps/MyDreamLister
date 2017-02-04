da252e68249eb6b3ebbd63d559be7035cc4e18dc
daxapps <daxapps777@gmail.com>
2017-02-04 10:27:25 -0600

    Add ItemCell.swift - connect labels to MainVC - add boiler plate TableVewCell functions to MainVC

---
+++ MyDreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  MyDreamLister
+//
+//  Created by Jason Crawford on 2/4/17.
+//  Copyright © 2017 Jason Crawford. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+
+
+}
